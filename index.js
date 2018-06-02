1652a0febffb143942d32cf652e3ce4c00feb8b6
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-02 22:29:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function shout(string) {
 const whisper = string => {
   return string.toLowerCase()
 }
+
+const logShout = string => {
+  console.log(string.toUpperCase())
+}
+
+const logWhisper = string => {
+  console.log(string.toLowerCase())
+}
+
+const sayHiToGrandma = string =>
+  string.toLowerCase() ? "I can't hear you" : null
+  
